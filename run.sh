126b8e011bbd22200e166fd5f9246c4a5129e106
RickyRSN <rickzz.corby@gmail.com>
2020-10-01 11:24:19 +0700

    cron auto

--- run.sh
+++ run.sh
@@ -1,8 +1,3 @@
 #! /bin/sh
 
-cd /home/ricky/my-iptv
-docker-compose down
-docker-compose up -d
-git add .
-git commit -m "cron auto"
-git push -u origin master
+cd /home/ricky/my-iptv && docker-compose down && docker-compose up && git add . && git commit -m "cron auto" && git push -u origin master
